4cd0a8b5ba89b15ef2aad2d09a62224626e95a5d
emivo <emil.airta@helsinki.fi>
2016-01-25 13:51:15 +0200

    sivuston rakentelua

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,22 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        View::make('home.html');
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        View::make('helloworld.html');
     }
-  }
+
+    public static function login() {
+        View::make('login.html');
+    }
+
+    public static function register() {
+        View::make('register.html');
+    }
+
+}
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,6 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Chef (
+id SERIAL PRIMARY KEY,
+name varchar(50) UNIQUE NOT NULL,
+pswrd varchar(50) NOT NULL
+);
