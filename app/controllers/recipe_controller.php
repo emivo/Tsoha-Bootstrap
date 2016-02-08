796ac3a739c414f993dac639d46471446aa14aba
emivo <emil.airta@helsinki.fi>
2016-02-08 21:54:26 +0200

    muutama bugi liiskattu

--- app/controllers/recipe_controller.php
+++ app/controllers/recipe_controller.php
@@ -25,41 +25,44 @@ class RecipeController extends BaseController {
         $params = $_POST;
 // kokki otetaan sessioista
 //        $chef_id = $_SESSION[array()];
-        
 //        Käydään myös ainesosat täytyy tallentaa siispä  tehdään niin että käydään parametrejä nii kauan läpi ette niitä ole enää
-        
+
         $recipe = new Recipe(array(
             'name' => $params['name'],
             'cooking_time' => $params['cooking_time'],
             'directions' => $params['directions'],
-            ));
-        
-    
+        ));
+
+
         $recipe_id = $recipe->save();
-        
-                
-        foreach($params['ingredient'] as $index => $row) {
+
+
+        foreach ($params['ingredient'] as $index => $row) {
             $ingredient = new Ingredient(array(
                 'recipe_id' => $recipe_id,
                 'name' => $row,
                 'quantity' => $params['quantity'][$index]
             ));
-            
+
             $ingredient->save();
         }
-        
+
         foreach ($params['keyword'] as $word) {
             $keyword = new Keyword(array(
-               'keyword' => $word 
+                'keyword' => $word
             ));
-            
+
             $keyword->save($recipe_id);
         }
-        
+
         Redirect::to('/recipe/' . $recipe->id, array('message' => 'Resepti on julkaistu'));
     }
-    
+
     public static function edit($id) {
+        $recipe = Recipe::find($id);
+        $comments = Comment::find_by_recipe_id($id);
+        $ingredients = Ingredient::find_by_recipe_id($id);
+        $keywords = Keyword::find_by_recipe_id($id);
         View::make('recipe/edit.html', array('recipe' => $recipe, 'comments' => $comments, 'ingredients' => $ingredients, 'keywords' => $keywords));
     }
 
